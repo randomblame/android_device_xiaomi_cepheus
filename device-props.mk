676c73cc8b7ce5c0c7ab1b8082f6e8b72f84483f
kelsi <dumbandroid@gmail.com>
2019-09-23 19:10:15 -0700

    no idea

--- device-props.mk
+++ device-props.mk
@@ -12,12 +12,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=440
-
-# Display features
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.displayfeature.histogram.enable=true \
-    ro.eyecare.brightness.threshold=11 \
-    ro.eyecare.brightness.level=5 \
-    ro.hist.brightness.threshold=7 \
-    sys.displayfeature.hbm.enable=true \
-    sys.displayfeature_hidl=true
